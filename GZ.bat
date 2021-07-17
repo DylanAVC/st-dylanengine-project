b360169f45f980682b011d9d85a534509fbfc511
DylanPews <77309014+DylanPews@users.noreply.github.com>
2021-07-16 21:00:55 -0400

    Create GZ.bat

---
+++ GZ.bat
@@ -0,0 +1,14 @@
+@echo off
+if exist "C:\Program Files\7-Zip\7z.exe" (
+    goto zip
+) else (
+    start "" http://www.7zip.org/download.html
+)
+
+:zip
+cd source/code
+"C:\Program Files\7-Zip\7z.exe" a -tzip "../windows/gameZip/game.love" .
+
+"../windows/gameZip/game.love"
+
+echo Done!
