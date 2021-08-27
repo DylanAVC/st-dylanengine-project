169848145cea290940946087d609c1dccae5ff60
DylanPews <77309014+DylanPews@users.noreply.github.com>
2021-08-27 19:56:42 -0400

    Add files via upload

--- code/main.lua
+++ code/main.lua
@@ -1,9 +1,16 @@
+print("Starting! \n")
+print("Loading Love Engine \n")
+local clockx = os.clock()
+local clocks = 0
+
 function love.load()
 	
 end
 
 function love.update(dt)
-	
+    if dt > 0.035 then return end
+    --for testing purposes. WARNING: this will print every time and it could be annoying
+    --print("Ticking " .. string.format( "time: %.2f", os.clock() - clockx) .. " Return Tick \n")
 end
 
 function love.draw()
---
+++ code/project.lua
@@ -0,0 +1,35 @@
+-- Lua version 5.1
+
+-- misc
+identity = nil
+appendidentity = false
+loveversion = "11.3"
+console = true
+accelerometerjoystick = true
+externalstorage = false
+gammacorrect = false
+-- audio
+mic = false
+mixwithsystem = false
+-- window
+tver = "1.0 "
+tdever = "| DE 1.0"
+tname = "Untitled "
+icon = nil
+width = 1280
+height = 720
+borderless = false
+resizeable = false
+minwidth = 1
+minheight = 1
+fullscreen = false
+fullscreentype = "desktop"
+vsync = 1
+msaa = 0
+depth = nil
+stencil = nil
+display = 1
+highdpi = false
+usedpiscale = true
+x = nil
+y = nil
